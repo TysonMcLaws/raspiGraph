ff0a07e334f166912155bbbb420792caf97b5e77
TysonMcLaws <47451713+TysonMcLaws@users.noreply.github.com>
2020-02-05 08:28:24 -0600

    Update getData.py

--- getData.py
+++ getData.py
@@ -18,6 +18,8 @@ while True:
 		break
  
 GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
 pin1 = 29 #GPIO 5
 pin2 = 31 #GPIO 6
 pin3 = 33 #GPIO 13
@@ -27,16 +29,18 @@ pin6 = 32 #GPIO 12
 pin7 = 36 #GPIO 16
 pin8 = 38 #GPIO 20
 pin9 = 40 #GPIO 21
+led = 7 #GPIO 4
 
-GPIO.setup(pin1, GPIO.IN)
-GPIO.setup(pin2, GPIO.IN)
-GPIO.setup(pin3, GPIO.IN)
-GPIO.setup(pin4, GPIO.IN)
-GPIO.setup(pin5, GPIO.IN)
-GPIO.setup(pin6, GPIO.IN)
-GPIO.setup(pin7, GPIO.IN)
-GPIO.setup(pin8, GPIO.IN)
-GPIO.setup(pin9, GPIO.IN)
+GPIO.setup(pin1, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin2, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin3, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin4, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin5, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin6, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin7, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin8, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(pin9, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(led, GPIO.OUT)
 
 dataFile = open("myData.txt", "w")
 dataFile.close()
@@ -52,39 +56,40 @@ while True:
 while True:
 	dataFile = open("myData.txt", "a")
 	message = ""
-	if (GPIO.input(pin1) == True):
+	GPIO.output(led, GPIO.HIGH)
+	if (GPIO.input(pin1) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin2) == True):
+	if (GPIO.input(pin2) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin3) == True):
+	if (GPIO.input(pin3) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin4) == True):
+	if (GPIO.input(pin4) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin5) == True):
+	if (GPIO.input(pin5) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin6) == True):
+	if (GPIO.input(pin6) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin7) == True):
+	if (GPIO.input(pin7) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin8) == True):
+	if (GPIO.input(pin8) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"
-	if (GPIO.input(pin9) == True):
+	if (GPIO.input(pin9) == HIGH):
 		message += "1,"
 	else:
 		message += "0,"	
@@ -97,4 +102,7 @@ while True:
 	dataFile.close()
 	
 	if d.now().second == int(duration):
+		GPIO.output(led, GPIO.LOW)
 		break	
+		
+GPIO.cleanup()
